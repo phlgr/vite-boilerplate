73b1840926d04466f8611c54ecac928bb6489ad9
phlgr <philipp@gartz.dev>
2021-08-16 12:22:12 +0200

    ✨ Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Server!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_request, response) => {
+  response.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
