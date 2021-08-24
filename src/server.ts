b41dacf0fb16309c4e37674135fbd1591bd11afb
phlgr <philipp@gartz.dev>
2021-08-24 12:21:59 +0200

    ✨ Create example api route

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get('/api/hello', (_request, response) => {
+  response.send('Hello API!');
+});
+
 app.use('/storybook', express.static('dist/storybook'));
 
 app.use(express.static('dist/app'));
