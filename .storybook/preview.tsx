c705b75b14f9d80bcc287d199ec9302f07532c58
phlgr <philipp@gartz.dev>
2021-08-24 14:22:06 +0200

    🔧 Add MemoryRouter to every story

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+
 import '../src/app/globals.css';
 
 export const parameters = {
@@ -9,3 +13,5 @@ export const parameters = {
     },
   },
 };
+
+addDecorator((story) => <MemoryRouter>{story()}</MemoryRouter>);
