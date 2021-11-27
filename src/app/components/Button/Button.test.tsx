562f1682a42ba4978bc573ad1d3cf3795924a2ce
lmachens <leon.machens@googlemail.com>
2021-11-27 12:25:51 +0100

    ✨ Add jest testing library to test components

---
+++ src/app/components/Button/Button.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Button from './Button';
+
+test('Button renders', () => {
+  const { queryByText } = render(<Button>Login</Button>);
+  expect(queryByText(/Login/i)).toBeTruthy();
+});
