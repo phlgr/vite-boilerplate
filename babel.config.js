ef5df68195b3e5326968d6a7324246ab1e64faa5
Jeremias Erbs <jerry@badabam.de>
2021-12-07 15:11:48 +0100

    Use babel config only for jest

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,15 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    [
-      '@babel/preset-react',
-      {
-        runtime: 'automatic',
-      },
-    ],
-    '@babel/preset-typescript',
-  ],
-};
+module.exports =
+  process.env.NODE_ENV === 'test'
+    ? {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          [
+            '@babel/preset-react',
+            {
+              runtime: 'automatic',
+            },
+          ],
+          '@babel/preset-typescript',
+        ],
+      }
+    : {};
